c98a0389cce899413913887c2354a4eceb1349fc
DevUt <javitri61@gmail.com>
2016-12-04 22:41:58 +0530

    [ALL FIXED] FIxed everything and adopted the new way to write()

--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int klog(int level,char a [ ] )
 {
@@ -7,7 +9,12 @@ int klog(int level,char a [ ] )
  int kmsg = open("/dev/kmsg", O_WRONLY);
   if (kmsg > 0) // Trying to open KMSG, if it doesn't return any error, continue executing the code.
   {
-  write(kmsg, "Test"); // Write "Test" to KMSG
+    char **buf_kmsg;
+
+    strcpy(buf_kmsg,"Test");
+    int size_in_buf = strlen(buf_kmsg) ;
+
+    write(kmsg,buf_kmsg,size_in_buf); // Write "Test" to KMSG
   } else {
 
     close(kmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ;
@@ -17,19 +24,25 @@ int klog(int level,char a [ ] )
       we would open another the file
       on sdcard to transmit our msg
     */
-    int skmsg = access(fname, F_OK ) ;  // Check if the file exists
-    if (skmsg == "-1") {  // If it doesn't exist, create it
-      sdkmsg = open("/sdcard/liblelog", O_CREAT); 
+    int sdkmsg = access("/sdcard/lible.log", F_OK ) ;  // Check if the file exists
+    if (sdkmsg == "-1") {  // If it doesn't exist, create it
+      sdkmsg = open("/sdcard/lible.log", O_CREAT);
       close(sdkmsg); // Close the discriptor with the value of "O_CREAT"
     }
 
     /*
       Checking is done, now we will write to the KMSG
     */
-      sdkmsg = open("/sdcard/liblelog",O_WRONLY) // OPEN TO WRITE
-      write(sdkmsg, "ERROR: COULD NOT OPEN KMSG PLEASE FIX ");// write errors
-      close(sdkmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ; 
+        char **buf_sd;
+      sdkmsg = open("/sdcard/lible.log",O_WRONLY); // OPEN TO WRITE
+
+      strcpy(buf_sd,"ERROR: COULD NOT OPEN KMSG PLEASE FIX");
+      int size_in_buf = strlen(buf_sd);
+
+      write(sdkmsg,buf_sd,size_in_buf);// write errors
+      close(sdkmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ;
 
 
 
+    }
 }
