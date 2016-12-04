c5eeaa515d3db0794bb3232a653dd84bb4bb5a40
Tim Wanders <Tim241@ziggo.nl>
2016-12-04 17:48:25 +0100

    Fixed grammar

--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -3,32 +3,31 @@
 
 int klog(int level,char a [ ] )
 {
-  // Open KMSG to write ;
+  // Write to kmsg if possibe.
  int kmsg = open("/dev/kmsg", O_WRONLY);
-  if (kmsg > 0) // CHECK IF OPENING KMSG DIDNT RETURN ANY ERRORS ;
+  if (kmsg > 0) // Trying to open KMSG, if it doesn't return any error, continue executing the code.
   {
-  write(kmsg, "Test"); // WRITE TO IT
+  write(kmsg, "Test"); // Write "Test" to KMSG
   } else {
 
     close(kmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ;
 
     /*
-      Now if the file doesnt exists
+      Now if the file doesn't exists
       we would open another the file
       on sdcard to transmit our msg
     */
-    int skmsg = access(fname, F_OK ) ;  // CHECK IF THE FILE EXISTS
-    if (skmsg == "-1") {  // IF IT DOESNT EXISTS THEN
-      sdkmsg = open("/sdcard/liblelog", O_CREAT); // CREATE IT
-      close(sdkmsg); // CLOSE THE DISCRIPTOR WITH O_CREAT VALUE
+    int skmsg = access(fname, F_OK ) ;  // Check if the file exists
+    if (skmsg == "-1") {  // If it doesn't exist, create it
+      sdkmsg = open("/sdcard/liblelog", O_CREAT); 
+      close(sdkmsg); // Close the discriptor with the value of "O_CREAT"
     }
 
     /*
-      CHECKING STAGE DONE
-      NOW WE WWOULD WRITE TO IT
+      Checking is done, now we will write to the KMSG
     */
       sdkmsg = open("/sdcard/liblelog",O_WRONLY) // OPEN TO WRITE
-      write(sdkmsg, "ERROR: COULD NOT OPEN KMSG PLEASE FIX ");// WRITE THE ERRORS
+      write(sdkmsg, "ERROR: COULD NOT OPEN KMSG PLEASE FIX ");// write errors
       close(sdkmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ; 
 
 
