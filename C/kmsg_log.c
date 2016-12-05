f02c30dec0e3506b4878848f38cd50fae4b11ebe
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-05 01:40:59 +0100

    Log C: Improvements
    - Silent unused var warnings,
    - Allocate buffers,
    - Close kmesg buffer everytime,
    - Provide a return value.

--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
-int klog(int level,char a [ ] )
+int klog(__attribute__((unused)) int level,__attribute__((unused)) char * a )
 {
   // Write to kmsg if possibe.
  int kmsg = open("/dev/kmsg", O_WRONLY);
+ int ret = -1; 
   if (kmsg > 0) // Trying to open KMSG, if it doesn't return any error, continue executing the code.
   {
-    char **buf_kmsg;
+    char *buf_kmsg = malloc(sizeof(char) * 256);
 
     strcpy(buf_kmsg,"Test");
     int size_in_buf = strlen(buf_kmsg) ;
 
-    write(kmsg,buf_kmsg,size_in_buf); // Write "Test" to KMSG
+    ret = write(kmsg,buf_kmsg,size_in_buf); // Write "Test" to KMSG
+    close(kmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ;
   } else {
 
-    close(kmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ;
 
     /*
       Now if the file doesn't exists
@@ -25,24 +28,29 @@ int klog(int level,char a [ ] )
       on sdcard to transmit our msg
     */
     int sdkmsg = access("/sdcard/lible.log", F_OK ) ;  // Check if the file exists
-    if (sdkmsg == "-1") {  // If it doesn't exist, create it
+
+    if (sdkmsg == -1) {  // If it doesn't exist, create it
       sdkmsg = open("/sdcard/lible.log", O_CREAT);
+
       close(sdkmsg); // Close the discriptor with the value of "O_CREAT"
     }
 
     /*
       Checking is done, now we will write to the KMSG
     */
-        char **buf_sd;
+      char *buf_sd = malloc(sizeof(char) * 256);
+
       sdkmsg = open("/sdcard/lible.log",O_WRONLY); // OPEN TO WRITE
 
       strcpy(buf_sd,"ERROR: COULD NOT OPEN KMSG PLEASE FIX");
       int size_in_buf = strlen(buf_sd);
 
-      write(sdkmsg,buf_sd,size_in_buf);// write errors
+
+      ret = write(sdkmsg,buf_sd,size_in_buf);// write errors
+
       close(sdkmsg); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ;
 
-
-
     }
+
+	return ret;
 }
