a8da7dcd8e4c342a92e8a8e065754d09d1284526
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-05 01:45:14 +0100

    Add test/debug binary

--- Android.mk
+++ Android.mk
@@ -1,9 +1,28 @@
-include $(CLEAR_VARS)
+LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles)
 
-LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
 LOCAL_MODULE    := liblelog
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := C/kmsg_log.c
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := liblelog_test
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := C/test.c
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
+
+LOCAL_SHARED_LIBRARIES := liblelog
+
+include $(BUILD_EXECUTABLE)
+
+
+
---
+++ C/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "kmsg_log.h"
+
+int main(void) {
+
+	return klog(0,"test");
+
+}
