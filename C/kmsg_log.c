7fa183f9070a819b8b3cf5743d4fe36ad9c7a7ff
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-05 15:16:53 +0100

    Log C: share same cursor for kmesg and file log
    - Share the same cursor for kmesg and file log (outbuf),
      this allow easily futur changes and more automated switch into both write method.

--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -14,27 +14,23 @@ int klog(__attribute__((unused)) int level,__attribute__((unused)) char * a )
  int ret = -1; 
 
   // Write to kmsg if possibe.
- int kmsg = open("/dev/kmsg", O_WRONLY);
-  if (kmsg > 0) // Trying to open KMSG, if it doesn't return any error, continue executing the code.
+ int outbuf = open("/dev/kmsg", O_WRONLY);
+  if (outbuf > 0) // Trying to open KMSG, 
   {
     strcpy(buf,"Test\n");
 
-    ret = write(kmsg, buf, strlen(buf) ); // Write to KMSG.
-    close(kmsg); // Close the file discriptor to avoid any leaks.
-  } else {
+  } else { //If kmesg don't open write to sd
     /*
       Now if the file doesn't exists, we would open another file on the sdcard to transmit our KMSG.
     */
-	int sdkmsg; 
-
     if (access(LOG_LOC, F_OK ) == -1) {  // Check if the file exists.
-      sdkmsg = open(LOG_LOC, O_CREAT | O_WRONLY); // If it doesn't exist, create it and open it to write to it.
+      outbuf = open(LOG_LOC, O_CREAT | O_WRONLY); // If it doesn't exist, create it and open it to write to it.
 
     } else {
 		/*
 		* TODO: Make better file checks.
 		*/
-	  sdkmsg = open(LOG_LOC, O_APPEND); // Else, assume file already exist: Open to append. 
+	  outbuf = open(LOG_LOC, O_APPEND); // Else, assume file already exist: Open to append. 
 	}
 
     /*
@@ -43,11 +39,11 @@ int klog(__attribute__((unused)) int level,__attribute__((unused)) char * a )
 
       strcpy(buf,"ERROR: COULD NOT OPEN KMSG PLEASE FIX");
 
-      ret = write(sdkmsg, buf, strlen(buf) );// Write any errors.
-
-      close(sdkmsg); // Close the file discriptor to avoid any leaks.
-
     }
+
+	ret = write(outbuf, buf, strlen(buf) ); // Write "Test" to KMSG
+
+	close(outbuf); // CLOSE THE FILE DISCRIPTOR TO AVOID LEAK ;
 
 	free(buf); // Empty buf memory.
 	return ret;
