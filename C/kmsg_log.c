af1b6aa62ac2455f96a17c34d5d143bca8aee8f6
DevUt <javitri61@gmail.com>
2016-12-06 22:19:28 +0530

    Add Copyright information

--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -1,3 +1,24 @@
+/*
+This file is part of lib-le-log
+
+ lib-le-log is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+
+ lib-le-log is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+Copyright © of Carbon-Fusion (www.github.com/Carbon-Fusion)
+*/
+
+
+*/
+/*
+ * Written / Contributed by 
+ * DevUt (www.github.com/DevUt)
+ * naiylk (www.github.com/nailyk-fr)
+ */
+  
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
