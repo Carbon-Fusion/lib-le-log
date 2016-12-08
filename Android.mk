674f3ecd3b19a968311e22def594216944a54098
DevUt <javitri61@gmail.com>
2016-12-08 16:15:30 +0530

    Fix Android.mk to build in Recovery && Fix LOCAL_PATH appearing after CLEAR VARS

--- Android.mk
+++ Android.mk
@@ -1,11 +1,22 @@
+
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 include $(call all-subdir-makefiles)
 
-LOCAL_PATH := $(call my-dir)
 LOCAL_MODULE    := liblelog
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 
+include $(BUILD_SHARED_LIBRARY)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+include $(call all-subdir-makefiles)
+
+LOCAL_MODULE    := liblelog
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
