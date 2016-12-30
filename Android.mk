6f7fd46467019571e57810d9476675f0f77dc156
DevUt <javitri61@gmail.com>
2016-12-30 11:02:48 +0530

    Change the way how modules are built  The module name here is changed this maybe "temporary"  what happened is , if we make a corresponding C++ file having the function klog then  it would cause weird symbol problems and make debugging difficult , and having different function name would be ridiculous.
    So ,
      The best option seems that to create different modules for the languages this way it solves the problem and also has the benifit that
      user could just include the bare minimum modules in their initramfs for the languages they require
    
    Situation Example,
      I have a lib that only utilizes C code
    
      If in this situation the lib-le-log has all languages built in one then it would just make my initramfs image large
      with no benifits
    
      But if the lib-le-log is broken down then it would be a small task and image
    
    Potential Problems ,
      A user which utilizes large Code of mixed languages may need to addd more SHARED_LIBRARIES in his Android.mk
    	BUT STILL THIS AINT A TEDIOUS TASk

--- Android.mk
+++ Android.mk
@@ -1,21 +1,25 @@
+##############################################
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-include $(call all-subdir-makefiles)
 
-LOCAL_MODULE    := liblelog
+LOCAL_MODULE    := liblelog_c
+LOCAL_SRC_FILES := kmsg_log.c
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 
 include $(BUILD_SHARED_LIBRARY)
 
+##############################################
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-include $(call all-subdir-makefiles)
 
-LOCAL_MODULE    := liblelog
+LOCAL_MODULE    := liblelog_c
+LOCAL_SRC_FILES := kmsg_log.c
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 
 include $(BUILD_SHARED_LIBRARY)
+###############################################
