1cd6bc056f97c20faa855bc38f9eec7376e9a211
DevUt <javitri61@gmail.com>
2016-12-30 13:47:43 +0530

    Fix lGPL clause

--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -1,4 +1,4 @@
-    This file is part of lib-le-log.
+ /*   This file is part of lib-le-log.
 
     lib-le-log is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
@@ -8,10 +8,12 @@
     lib-le-log is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
     along with lib-le-log.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 /*
 Copyright © of Carbon-Fusion (www.github.com/Carbon-Fusion)
 */
