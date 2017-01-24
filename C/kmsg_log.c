d15d715668694cff8898fe720f32ebbd2edbadba
DevUt <javitri61@gmail.com>
2017-01-24 16:13:03 +0530

    Fix pointer bug in strcpy and change the way of concentating the tag and message
    The new way of concenatating is more sane and safe work
    
    In the new way we allocate the string literal a space then strcpy the tag into it and strcat the the true message with the tag
    
    In the end when klog finishes and returns a value(int) it would first free the buffer to avoid memeory cluttering and then return the value that klog returned in the first place

--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -13,14 +13,14 @@
 int klog(char *msg)
 {
 
- char *buf = malloc(sizeof(char) * 256);
+ char *buf = malloc(256);
  int ret = -1;
 
   // Write to kmsg if possibe.
  int outbuf = open("/dev/kmsg", O_WRONLY);
   if (outbuf > 0) // Trying to open KMSG,
   {
-    strcpy(buf,*msg);
+    strcpy(buf,msg);
   } else { //If kmesg don't open write to sd
     /*
       Now if the file doesn't exists, we would open another file on the sdcard to transmit our KMSG.
@@ -51,32 +51,59 @@ int klog(char *msg)
 	return ret;
 }
 
-	int logwarn(char *warning)
-	{
-		char *warn ="Warning! : "  ;
-		strcat(*warn,*warning);
-		int ret = klog(*warn);
-		return(ret);
-	}
-
-	int logerror(char *error)
-	{
-		char *error_tag ="Error! : "  ;
-		strcat(*error_tag,error);
-		int ret = klog(*error_tag);
-		return(ret);
-	}
-	int logdebug (char *debug_msg)
-	{
-		char *debug_tag = "Debug : ";
-		strcat(*debug_tag,debug_msg);
-		int ret = klog(*debug_tag);
-		return(ret);
-	}
-	int loginfo (char *info)
-	{
-		char *info_tag = "Info : ";
-		strcat(*info_tag,info);
-		int ret = klog(*info_tag);
-		return(ret);
-	}
+/*
+  This functions prefixes the Warning label
+  to the real message and sends it to klog
+  for real "printing" to kmsg (IF LOG_LOC not defined or used)
+*/
+int logwarn(char *warning)
+{
+ char *warn = malloc(256);
+  strcpy(warn ,"Warning!: ");
+  strcat(warn ,warning);
+ int ret = klog(warn) ;
+  return(ret);
+  free(warn);
+ }
+  /*
+    This functions prefixes the Error label
+    to the real message and sends it to klog
+    for real "printing" to kmsg (IF LOG_LOC not defined or used)
+  */
+int logerror(char *error_msg)
+{
+ char *error = malloc(256);
+  strcpy(error,"Error!: ");
+  strcat(error,error_msg);
+ int ret = klog(error);
+  return(ret);
+  free(error);
+ }
+  /*
+    This functions prefixes the debug label
+    to the real message and sends it to klog
+    for real "printing" to kmsg (IF LOG_LOC not defined or used)
+  */
+int logdebug (char *debug_msg)
+{
+ char *debug = malloc(256);
+  strcpy(debug,"Error!: ");
+  strcat(debug,debug_msg);
+ int ret = klog(debug);
+  return(ret);
+  free(debug);
+ }
+  /*
+    This functions prefixes the Info label
+    to the real message and sends it to klog
+    for real "printing" to kmsg (IF LOG_LOC not defined or used)
+  */
+int loginfo (char *info_msg)
+{
+ char *info = malloc(256);
+  strcpy(info,"Error!: ");
+  strcat(info,info_msg);
+ int ret = klog(info);
+  return(ret);
+  free(info);
+ }
