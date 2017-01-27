f79c16d2407bc89d03f6ffeded1de3c020aeb93b
DevUt <javitri61@gmail.com>
2017-01-27 15:59:09 +0530

    Add option to Wrap log tags and fix assignment of buf
    After some thought it was decided that the log tags should all be of the same length
    > but for some it would become too complex of understanding I ( considering a noob user reading the log)
    
    So it was decided to add a flag that can change the way it behaves
    
     So we added the LOG_WRAP_TAGS flag ,so that the use can the dev can enable it in the makefiles and have the shortened tags with Info as I: Warn as W: Error as E: and Debug as D:
    
    We also fixed assignment of buf , it was getting assigned to a string literal while being a buffer, so we utilized strcpy over here to assign it

--- C/Android.mk
+++ C/Android.mk
@@ -1,2 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+ifeq ($(LOG_WRAP_TAGS),true)
+	LOCAL_CFLAGS += -DLOG_WRAP_TAGS
+endif
 LOCAL_SRC_FILES := kmsg_log.c
--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -11,14 +11,14 @@
 /*
   This is the main function "klog"
   it takes the the log level
-  ==================================== \
-   label  | loglevel | message         /
-  ==================================== \
-  | Info  | 0        |  Info :         /
-  | Warn  | 1        |  Warn :         \
-  | Error | 2        |  Error :        /
-  | Debug | 3        |  Debug :        \
-  ==================================== /
+  ======================================\
+   label  | loglevel | message          /
+  ======================================\
+  | Info  | 0        |  Info :    || I: /
+  | Warn  | 1        |  Warn :    || W: \
+  | Error | 2        |  Error :   || E: /
+  | Debug | 3        |  Debug :   || D: \
+  ===================================== /
   then opens the /dev/kmsg or LOG_LOC
   if it cant acces the location
   it tries to open the LOG_LOC
@@ -35,20 +35,43 @@ int klog(int level, char *msg)
   {
     switch (level)
      {
-      case 0 : buf = "Info :" ;
-                strcat(buf,msg);
-                break ;
+      case 0 :
+              #ifdef LOG_WRAP_TAGS
+                  strcpy(buf,"I:");
+                  strcat(buf,msg);
+              #else
+                  strcpy(buf,"Info :");
+                  strcat(buf,msg);
+              #endif
 
-      case 1 : buf = "Warn :";
-                strcat(buf,msg);
+      case 1 :
+              #ifdef LOG_WRAP_TAGS
+                  strcpy(buf,"W:");
+                  strcat(buf,msg);
+              #else
+                  strcpy(buf,"Warn :");
+                  strcat(buf,msg);
+              #endif
+
+      case 2 :
+              #ifdef LOG_WRAP_TAGS
+                  strcpy(buf,"E:");
+                  strcat(buf,msg);
+              #else
+                  strcpy(buf,"Error:");
+                  strcat(buf,msg);
+              #endif
+
+      case 3 :
+              #ifdef LOG_WRAP_TAGS
+                  strcpy(buf,"D:");
+                  strcat(buf,msg);
+              #else
+                  strcpy(buf,"Debug:");
+                  strcat(buf,msg);
+              #endif
               break ;
-      case 2 : buf = "Error :"
-                strcat(buf,msg);
-              break ;
-      case 3 : buf = "Debug :"
-                strcat(buf,msg);
-              break ;
-      default : buf = "Error : Unknown log level "
+      default : strcpy(buf,"Error : Unknown log level ");
                 break;
     }
 
