70b48abc1d5288b7ab68f993ee76e5f16bc9ac28
DevUt <javitri61@gmail.com>
2017-01-28 20:48:01 +0530

    Add header DEAFULT_LOG_LOC and change LOG_LOc to ALT_LOG_LOC
    Previously if the user wanted open anything else other than /dev/kmsg , he couldnt. He had to edit the source for changing the fle to be opned. Now he could just add DEFAULT_LOG_LOC to his device specific makefile to oprn a particular location to write. In the same way he can also now define ATL_LOG_lOC to open a file if /dev/kmsg fails
    
    By default DEFAULT_LOG_LOC is /dev/kmsg and ALT_LOG_LOC is /cache/lible.log

--- C/Android.mk
+++ C/Android.mk
@@ -2,4 +2,8 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(LOG_WRAP_TAGS),true)
 	LOCAL_CFLAGS += -DLOG_WRAP_TAGS
 endif
+
+ifeq ($(DEFAULT_LOG_LOC),)
+	LOCAL_CFLAGS += -DDEFAULT_LOG_LOC=\"$(DEFAULT_LOG_LOC)\"
+endif
 LOCAL_SRC_FILES := kmsg_log.c
--- C/kmsg_log.c
+++ C/kmsg_log.c
@@ -5,9 +5,14 @@
 #include <fcntl.h>
 #include <string.h>
 
-#ifndef LOG_LOC
- #define LOG_LOC "/cache/lible.log"
+#ifndef ALT_LOG_LOC
+ #define ALT_LOG_LOC "/cache/lible.log"
 #endif
+
+#ifndef DEFAULT_LOG_LOC
+  #define  DEFAULT_LOG_LOC "/dev/kmsg"
+#endif
+
 /*
   This is the main function "klog"
   it takes the the log level
@@ -19,10 +24,10 @@
   | Error | 2        |  Error :   || E: /
   | Debug | 3        |  Debug :   || D: \
   ===================================== /
-  then opens the /dev/kmsg or LOG_LOC
-  if it cant acces the location
-  it tries to open the LOG_LOC
-  and print COULD NOT OPEN KMSG PLEASE FIX
+  then opens the /dev/kmsg or DEFAULT_LOG_LOC (if defined) ,
+  if it cant acces the location ,
+  it tries to open the ALT_LOG_LOC ,
+  and writes "COULD NOT OPEN KMSG PLEASE FIX".
 */
 int klog(int level, char *msg)
 {
@@ -30,17 +35,17 @@ int klog(int level, char *msg)
  int ret = -1;
  char buf [256];
 
- int outbuf = open("/dev/kmsg", O_WRONLY);
+ int outbuf = open(DEFAULT_LOG_LOC, O_WRONLY);
   if (outbuf > 0)
       ;
    else {
-    if (access(LOG_LOC, F_OK ) == -1) {
-      outbuf = open(LOG_LOC, O_CREAT | O_WRONLY);
+    if (access(ALT_LOG_LOC, F_OK ) == -1) {
+      outbuf = open(ALT_LOG_LOC, O_CREAT | O_WRONLY);
     } else {
 		/*
 		* TODO: Make better file checks.
 		*/
-	  outbuf = open(LOG_LOC, O_APPEND);
+	  outbuf = open(ALT_LOG_LOC, O_APPEND);
       strcpy(buf,"COULD NOT OPEN KMSG PLEASE FIX");
     }
   }
